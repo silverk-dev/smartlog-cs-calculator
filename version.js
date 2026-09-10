d66299625c6f2f4bb5206753923a5e3d98755c91
silverk-dev <silverk@squares.ai>
2026-09-10 13:34:49 +0900

    Update version.js

--- version.js
+++ version.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 window.APP_META = Object.freeze({
-  version: '1.1.0',
+  version: '1.0.0',
   releaseDate: '2026-09-10',
   pricingYear: '2026'
 });
